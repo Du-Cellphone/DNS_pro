f5594240ee774b21fdeee0e1a8e8c3cf7cf16e86
Du-Cellphone <dxf20050307@gmail.com>
2026-04-12 19:54:20 +0800

    - Refactor DNS Cache to use XXH3 for hashing - Improve thread safety with shard initialization - Bind each shard  to a certain thread, improving performances by eliminating race conditions.

--- DNS_Cache.cpp
+++ DNS_Cache.cpp
@@ -1,9 +1,14 @@
 #include "DNS_Cache.h"
-#include <chrono>
 #include <cstddef>
 #include <memory>
 #include <optional>
 #include <vector>
+
+namespace Cache
+{
+
+
+thread_local int my_shard_id = -1;
 
 DNS_Cache::DNS_Cache()
 {
@@ -13,62 +18,54 @@ DNS_Cache::DNS_Cache()
     }
 }
 
+
+void DNS_Cache::initialize_thread_shard(int id)
+{
+    my_shard_id = id % SHARD_CNT;
+}
+
+
 std::optional<IPAddress> DNS_Cache::get(const std::string &domain, const TimePoint &now)
 {
-    size_t shard_id = get_shard_id(domain);
-    return cache_shards[shard_id]->get(domain, now);
+    return cache_shards[my_shard_id]->get(domain, now);
 }
+
 
 void DNS_Cache::put(const std::string &domain, const IPAddress &ip, const TimePoint &now)
 {
-    size_t shard_id = get_shard_id(domain);
-    cache_shards[shard_id]->put(domain, ip, now);
+    cache_shards[my_shard_id]->put(domain, ip, now);
 }
 
-size_t DNS_Cache::get_shard_id(const std::string &domain) const
-{
-    DomainHash hash;
-    return hash(domain) % SHARD_CNT;
-}
 
 std::optional<IPAddress> DNS_Cache::CacheShard::get(const std::string &domain, const TimePoint &now)
 {
-    slock.lock();
 
     Hash_Table::iterator it = hash_table.find(domain);
-    if (it == hash_table.end() || now > entries[it->second].expiry)
-    {
-        slock.unlock();
+    if (it == hash_table.end() || now > entries[it->second].expiry) // TODO 如果请求TYPE_A但缓存里是TYPE_AAAA，这样的情况目前是无法命中的
         return std::nullopt;
-    }
+
     CacheEntry &entry = entries[it->second];
     entry.chance      = true;
-    IPAddress result  = entry.ip;
-    slock.unlock();
-    return result;
+    return entry.ip;
 }
+
 
 void DNS_Cache::CacheShard::put(const std::string &domain, const IPAddress &ip, const TimePoint &now)
 {
-    slock.lock();
-
     Hash_Table::iterator it = hash_table.find(domain);
     if (it != hash_table.end()) // 已在缓存中，则更新一下
     {
         CacheEntry &entry = entries[it->second];
         entry.ip          = ip;
-        entry.ip_len      = ip.len;
         entry.expiry      = now + DEFAULT_TTL;
         entry.chance      = true;
-        slock.unlock();
         return;
     }
 
     if (entries.size() < entries.capacity()) // entries未满
     {
         hash_table[domain] = entries.size();
-        entries.emplace_back(domain, ip, ip.len, now + DEFAULT_TTL);
-        slock.unlock();
+        entries.emplace_back(domain, ip, now + DEFAULT_TTL);
         return;
     }
 
@@ -85,12 +82,10 @@ void DNS_Cache::CacheShard::put(const std::string &domain, const IPAddress &ip, 
 
             current.domain = domain;
             current.ip     = ip;
-            current.ip_len = ip.len;
             current.expiry = now + DEFAULT_TTL;
             current.chance = true;
 
             hand = (hand + 1) % entries.size();
-            slock.unlock();
             return;
         }
         else
@@ -99,5 +94,14 @@ void DNS_Cache::CacheShard::put(const std::string &domain, const IPAddress &ip, 
             hand           = (hand + 1) % entries.size();
         }
     }
-    slock.unlock();
 }
+
+
+DNS_Cache::CacheShard::CacheShard()
+{
+    hash_table.reserve(TABLE_SIZE / 0.75); // 负载因子0.75
+    entries.reserve(TABLE_SIZE);
+}
+
+
+} // namespace Cache
---
+++ HashUtils.hpp
@@ -0,0 +1,91 @@
+#pragma once
+
+#include <cstddef>
+#include <string>
+#include <cstddef>
+#include <cstdint>
+#include <nmmintrin.h>
+#include <string_view>
+#define XXH_INLINE_ALL
+#include <xxhash.h>
+
+
+struct DomainHash
+{
+    size_t operator()(const std::string &d) const
+    {
+        const char *data = d.data();
+        size_t      len  = d.size();
+
+        uint64_t crc = 0;
+
+        while (len >= 8)
+        {
+            crc = _mm_crc32_u64(crc, *reinterpret_cast<const uint64_t *>(data));
+            data += 8;
+            len -= 8;
+        }
+
+        while (len >= 4)
+        {
+            crc = _mm_crc32_u32(crc, *reinterpret_cast<const uint32_t *>(data));
+            data += 4;
+            len -= 4;
+        }
+
+        while (len >= 2)
+        {
+            crc = _mm_crc32_u16(crc, *reinterpret_cast<const uint16_t *>(data));
+            data += 2;
+            len -= 2;
+        }
+
+        if (len >= 1)
+            crc = _mm_crc32_u8(crc, *reinterpret_cast<const uint8_t *>(data));
+
+        return static_cast<size_t>(crc);
+    }
+};
+
+struct SvCRC32
+{
+    size_t operator()(std::string_view sv) const noexcept
+    {
+        const char *data = sv.data();
+        size_t      len  = sv.size();
+        uint64_t    crc  = 0;
+        while (len >= 8)
+        {
+            crc = _mm_crc32_u64(crc, *reinterpret_cast<const uint64_t *>(data));
+            data += 8;
+            len -= 8;
+        }
+        while (len >= 4)
+        {
+            crc = _mm_crc32_u32(crc, *reinterpret_cast<const uint32_t *>(data));
+            data += 4;
+            len -= 4;
+        }
+        while (len >= 2)
+        {
+            crc = _mm_crc32_u16(crc, *reinterpret_cast<const uint16_t *>(data));
+            data += 2;
+            len -= 2;
+        }
+        if (len)
+        {
+            crc = _mm_crc32_u8(crc, *reinterpret_cast<const uint8_t *>(data));
+        }
+        return static_cast<size_t>(crc);
+    }
+};
+
+struct XXH3_64
+{
+    size_t operator()(const std::string_view &sv) const noexcept { return static_cast<size_t>(XXH3_64bits(sv.data(), sv.size())); }
+};
+
+inline XXH64_hash_t xxH3_64bits(const std::string_view sv)
+{
+    return XXH3_64bits(sv.data(), sv.size());
+}
